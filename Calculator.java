55b26ffafdd15519f204f00814ea36d9aa605103
LlamaSlurper <LlamaSlurper@gmail.com>
2023-12-21 20:27:10 -0600

    Started Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+class Calculator {
+    public static void main(String[] args) {   
+
+    }
+}
+
+
+//Using classes
+class Operations {
+    private char operatorChar;
+
+    //using Constructors
+    public Operations (char operatorChar) {
+        this.operatorChar = operatorChar;
+    } 
+
+    public char operatorCharGetter () {
+        return this.operatorChar;
+    }
+}
